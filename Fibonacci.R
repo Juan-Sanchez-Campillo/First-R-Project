a4d28b271be6cd9c42b2bb8bab09e0ef125664f0
Juan <juan.sanchez.campillo@outlook.com>
2024-02-17 01:25:46 +0100

    first commit

---
+++ Fibonacci.R
@@ -0,0 +1,18 @@
+# This is the first R project I am going to create 
+
+print_fibonacci <- function(n) {
+  a <- 0
+  b <- 1
+  
+  cat("Fibonacci Sequence:")
+  for (i in 1:n) {
+    cat(a, " ")
+    next_num <- a + b
+    a <- b
+    b <- next_num
+  }
+}
+
+# Example usage
+number_of_terms <- 10
+print_fibonacci(number_of_terms)
